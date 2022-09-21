6d1987eca24ad82ea2e6a71709b6dbd5391e1992
mamunur1296 <programer597@gmail.com>
2022-09-21 12:11:56 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Mamun is my name and i bacome a programmer</h1>
+        <h1>it is a man </h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
